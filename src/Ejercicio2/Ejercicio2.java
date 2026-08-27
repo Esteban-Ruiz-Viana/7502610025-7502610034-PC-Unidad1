d2588df167ecda8e139a06ddf1a957b3e816ed17
Maria Fernanda <mvasquezc4@unicartagena.edu.co>
2026-08-26 20:08:50 -0500

    feat: Taller 2, Ejercicio 2(Correcion)

--- src/Ejercicio2/Ejercicio2.java
+++ src/Ejercicio2/Ejercicio2.java
@@ -1,4 +1,13 @@
 package Ejercicio2;
 
 public class Ejercicio2 {
+
+    public static void main(String[] args) {
+        int a = 10, b = 5;
+
+        System.out.println("Suma: " + Matematicas.sumar(a, b));
+        System.out.println("Resta: " + Matematicas.restar(a, b));
+        System.out.println("Multiplicación: " + Matematicas.multiplicar(a, b));
+        System.out.println("División: " + Matematicas.dividir(a, b));
+    }
 }
