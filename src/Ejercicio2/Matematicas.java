cd98cdbae7543f164ae0a04e9182b4ead2af2b52
fernanda <mvasquezc4@unicartagena.edu.co>
2026-08-26 20:08:50 -0500

    feat: Taller 2, Ejercicio 2(Correcion)

--- src/Ejercicio2/Matematicas.java
+++ src/Ejercicio2/Matematicas.java
@@ -1,4 +1,23 @@
 package Ejercicio2;
 
 public class Matematicas {
-}
+
+        public static int sumar(int a, int b) {
+            return a + b;
+        }
+
+        public static int restar(int a, int b) {
+            return a - b;
+        }
+
+        public static int multiplicar(int a, int b) {
+            return a * b;
+        }
+
+        public static double dividir(int a, int b) {
+            if (b == 0) {
+                throw new ArithmeticException("No se puede dividir entre cero");
+            }
+            return (double) a / b;
+        }
+    }
